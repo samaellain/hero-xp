a049a5de1b329b73db1896ab18d402530cc8c71c
Jéferson Nunes <dark.samael@gmail.com>
2024-04-26 00:31:53 -0300

    Quick XP

---
+++ HeroXP.js
@@ -0,0 +1,23 @@
+let heroXP = ["Samael", 7000]
+let xp = heroXP[1]
+let nivel
+
+if (xp <= 1000) {
+    nivel = "Ferro";
+} else if (xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp <= 5000) {
+    nivel = "Prata";
+} else if (xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp <= 8000) {
+    nivel = "Platina";
+} else if (xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log("O Herói de nome " + heroXP[0] + " está no nível de " + nivel);
